cee7a5c35c9cee7c3e712855d3f9500245163e50
ViktorTarasevich <36436176+ViktorTarasevich@users.noreply.github.com>
2023-01-16 11:01:41 +0300

    Delete src directory

--- src/cli/args.js
+++
@@ -1,8 +0,0 @@
-const parseArgs = () => {
-    // Write your code here
-    process.argv.forEach((index, arg) => {
-        console.log(`propName is ${index}`);
-    });
-};
-
-parseArgs();
--- src/streams/write.js
+++
@@ -1,8 +0,0 @@
-import fs from 'fs';
-const write = async () => {
-    // Write your code here
-    const writableStreamNew = 'This file should be read using Streams API write'
-    const writableStream = fs.createWriteStream('./src/streams/files/fileToWrite.txt');
-        writableStream.write(writableStreamNew)
-};
-await write();
